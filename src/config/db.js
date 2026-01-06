3545327bb437425b36b354d3d4943aaa58126b93
Md. Sadiar Rahman <sadiar.rahman970@gmail.com>
2026-01-06 17:55:05 +0600

    update db

--- src/config/db.js
+++ src/config/db.js
@@ -6,13 +6,24 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT, // পোর্টটি যোগ করা হয়েছে
+    port: Number(process.env.DB_PORT) || 20403, // Number-এ রূপান্তর করা হলো
     ssl: {
-        rejectUnauthorized: false // Aiven এর জন্য এটি অবশ্যই লাগবে
+        rejectUnauthorized: false 
     },
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    connectTimeout: 20000 // ২০ সেকেন্ড সময় দিন কানেক্ট হতে
+});
+
+// কানেকশন চেক করার জন্য একটি টেস্ট
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error("ডাটাবেস কানেকশন এরর:", err.message);
+    } else {
+        console.log("ডাটাবেস কানেকশন সফল হয়েছে!");
+        connection.release();
+    }
 });
 
 module.exports = pool.promise();
