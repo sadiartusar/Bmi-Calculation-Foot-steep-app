143fd33ebb48f7cd23ed31739baf2f90448bb516
Md. Sadiar Rahman <sadiar.rahman970@gmail.com>
2026-01-06 17:12:41 +0600

    update env & db

--- src/config/db.js
+++ src/config/db.js
@@ -6,6 +6,10 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    port: process.env.DB_PORT, // পোর্টটি যোগ করা হয়েছে
+    ssl: {
+        rejectUnauthorized: false // Aiven এর জন্য এটি অবশ্যই লাগবে
+    },
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
